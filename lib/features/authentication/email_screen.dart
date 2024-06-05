4c072dd684c25bbdfcf4a436100d5cb4f9c56197
Eunhui <beensy74@gmail.com>
2024-06-05 00:13:04 -0400

    Update email_screen.dart

--- lib/features/authentication/email_screen.dart
+++ lib/features/authentication/email_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/customize_screen_part1.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
 class EmailScreen extends StatefulWidget {
   const EmailScreen({super.key});
@@ -12,6 +14,12 @@ class EmailScreen extends StatefulWidget {
 
 class _EmailScreenState extends State<EmailScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _bdayController = TextEditingController();
+  String _email = "";
+  String _name = "";
+  final String _bday = "";
   Map<String, String> formData = {};
   void _onSubmitTap() {
     if (_formKey.currentState != null) {
@@ -22,60 +30,140 @@ class _EmailScreenState extends State<EmailScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: FaIcon(
-          FontAwesomeIcons.twitter,
-          color: Theme.of(context).primaryColor,
-          size: Sizes.size36,
-        ),
+  void initState() {
+    super.initState();
+    _nameController.addListener(() {
+      setState(() {
+        _name = _nameController.text;
+      });
+    });
+    _emailController.addListener(() {
+      setState(() {
+        _email = _emailController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  void onEmailTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const CustomizeScreenPart1(),
       ),
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gaps.v40,
-            const Text(
-              "Create your account",
-              style: TextStyle(
-                fontSize: Sizes.size28,
-                fontWeight: FontWeight.w800,
+    );
+  }
+
+  bool _isEmailValid() {
+    if (_email.isEmpty) return false;
+    final regExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!regExp.hasMatch(_email)) {
+      return false;
+    }
+    return true;
+  }
+
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onSubmit() {
+    if (_email.isEmpty || _isEmailValid() != null) return;
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const CustomizeScreenPart1(),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: FaIcon(
+            FontAwesomeIcons.twitter,
+            color: Theme.of(context).primaryColor,
+            size: Sizes.size36,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              const Text(
+                "Create your account",
+                style: TextStyle(
+                  fontSize: Sizes.size28,
+                  fontWeight: FontWeight.w800,
+                ),
               ),
-            ),
-            Gaps.v8,
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  Gaps.v28,
-                  TextFormField(
+              Gaps.v8,
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Gaps.v28,
+                    TextFormField(
+                        controller: _nameController,
+                        decoration: const InputDecoration(
+                          hintText: "Name",
+                        ),
+                        validator: (value) {
+                          if (value != null && value.isEmpty) {
+                            return "Check your name";
+                          }
+                          return null;
+                        },
+                        onSaved: (newValue) {
+                          if (newValue != null) {
+                            formData['name'] = newValue;
+                          }
+                        }),
+                    TextFormField(
+                      controller: _emailController,
                       decoration: const InputDecoration(
-                        hintText: "Name",
+                        hintText: "Email",
                       ),
                       validator: (value) {
-                        return " I don't like your name";
+                        if (value != null) {
+                          if (value.isEmpty || !_isEmailValid()) {
+                            return "Check your Email";
+                          }
+                        }
+                        return null;
                       },
                       onSaved: (newValue) {
                         if (newValue != null) {
-                          formData['name'] = newValue;
+                          formData['email'] = newValue;
                         }
-                      }),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Email",
+                      },
                     ),
-                    validator: (value) {
-                      return " I don't like your email";
-                    },
-                    onSaved: (newValue) => print(newValue),
-                  ),
-                ], // have to make email field, button
-              ),
-            )
-          ],
+                  ], // have to make email field, button
+                ),
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          elevation: 0,
+          child: GestureDetector(
+            onTap: _onSubmitTap,
+            child: FormButton(
+              disabled: (_name.isEmpty || _email.isEmpty || !_isEmailValid()),
+            ),
+          ),
         ),
       ),
     );
