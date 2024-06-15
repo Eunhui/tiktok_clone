6ca617876c4a29222517d525b7931c5dfdf4fe5b
Eunhee <beensy74@gmail.com>
2024-06-14 22:55:13 -0500

    Update discover_screen.dart

--- lib/views/discover_screen.dart
+++ lib/views/discover_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:threads/constants/sizes.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({Key? key}) : super(key: key);
@@ -8,8 +11,22 @@ class DiscoverScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('discover'),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Search",
+            style:
+                TextStyle(fontSize: Sizes.size32, fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: const Column(
+          children: [
+            CupertinoSearchTextField(),
+          ],
+        ),
+      ),
     );
   }
 }
